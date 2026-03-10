fb6465965e05f6106383776eafb23a85bed6c27e
Juan Pablo Millicay <juan22milli@gmail.com>
2026-03-10 11:42:48 -0300

    crear esquema y modelo

--- app.js
+++ app.js
@@ -4,3 +4,18 @@ mongoose.connect('mongodb+srv://grupo-12:grupo-12@cluster0.blryo.mongodb.net/Nod
 .then(()=> console.log('Conexión exitosa a MongoDB'))
 .catch(error=>console.error('Error al conectar a MongoDB:', error));
 
+//Crea Esquema y modelo de la BD Superhéroes
+const superheroSchema= new mongoose.Schema({
+    nombreSuperHeroe:{type:String, required:true},
+    nombreReal:{type:String, required:true},
+    edad:{type:Number, min:0},
+    planetaOrigen:{type:String, default:'Desconocido'},
+    debilidad: String,
+    poderes: [String],
+    aliados: [String],
+    enemigos: [String],
+    createdAt: {type: Date, default: Date.now},
+    creador: String,
+},{collection: 'Grupo-12'});
+
+const SuperHero= mongoose.model('SuperHero', superheroSchema);
